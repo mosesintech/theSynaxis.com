e9b8f8a63f31120f83247d5f6010a613e8c7ef4a
Moses Maximino Cosme II <mosesmaxcosme@gmail.com>
2022-01-09 01:12:41 -0600

    complete modules query

--- src/fragments.ts
+++ src/fragments.ts
@@ -7,72 +7,443 @@ export const fragments = graphql`
       contentModule {
         ... on WpDefaultTemplate_Flexiblecontentmodules_ContentModule_Banner {
           fieldGroupName
+          bannerTitle
+          bannerContent
+          bannerBackgroundColor
+          bannerLink {
+            target
+            title
+            url
+          }
         }
         ... on WpDefaultTemplate_Flexiblecontentmodules_ContentModule_CallToAction {
           fieldGroupName
+          ctaTitle
+          ctaContent
+          ctaPhoneNumber
+          ctaBackgroundColor
+          ctaButton {
+            target
+            title
+            url
+          }
         }
         ... on WpDefaultTemplate_Flexiblecontentmodules_ContentModule_Carousel {
           fieldGroupName
+          carouselTitle
+          carouselText
+          carouselBackgroundColor
+          carouselCard {
+            title
+            text
+            link {
+              target
+              title
+              url
+            }
+            image {
+              altText
+            }
+          }
+          carouselButton {
+            target
+            title
+            url
+          }
         }
         ... on WpDefaultTemplate_Flexiblecontentmodules_ContentModule_CompareProducts {
           fieldGroupName
+          compareProductsTitle
+          compareProductsBackgroundColor
+          switchTitle
+          switchText
+          switchBackgroundColor
+          switchOptions {
+            title
+            text
+            link {
+              target
+              title
+              url
+            }
+          }
+          selectionTitle
+          selectionText
+          selectionBackgroundColor
+          selectionOptions {
+            optionCards {
+              optionOne {
+                title
+                text
+                link {
+                  target
+                  title
+                  url
+                }
+                image {
+                  altText
+                }
+              }
+              optionTwo {
+                title
+                text
+                link {
+                  target
+                  title
+                  url
+                }
+                image {
+                  altText
+                }
+              }
+              optionImage {
+                altText
+              }
+            }
+          }
         }
         ... on WpDefaultTemplate_Flexiblecontentmodules_ContentModule_DataTable {
           fieldGroupName
+          dataTableTitle
+          dataTableText
+          dataTableBackgroundColor
+          dataTableButton {
+            target
+            title
+            url
+          }
+          dataTable {
+            columns {
+              heading
+              row {
+                item
+              }
+            }
+          }
         }
         ... on WpDefaultTemplate_Flexiblecontentmodules_ContentModule_FeatureLinks {
           fieldGroupName
+          featureLinksTitle
+          featureLinksText
+          featureLinksBackgroundColor
+          featureLinksButton {
+            target
+            title
+            url
+          }
+          featureLinks {
+            title
+            text
+            image {
+              altText
+            }
+            button {
+              target
+              title
+              url
+            }
+          }
         }
         ... on WpDefaultTemplate_Flexiblecontentmodules_ContentModule_Gallery {
           fieldGroupName
+          galleryTitle
+          galleryText
+          galleryBackgroundColor
+          galleryButton {
+            target
+            title
+            url
+          }
+          imageGallery {
+            altText
+          }
         }
         ... on WpDefaultTemplate_Flexiblecontentmodules_ContentModule_Hero {
           fieldGroupName
+          heroTitle
+          heroText
+          heroBackgroundColor
+          heroBackgroundVideo
+          heroBackgroundImage {
+            altText
+          }
+          heroButton {
+            target
+            title
+            url
+          }
+          heroGallery {
+            altText
+          }
         }
         ... on WpDefaultTemplate_Flexiblecontentmodules_ContentModule_Intro {
           fieldGroupName
+          introTitle
+          introContent
+          introBackgroundColor
+          introButton {
+            target
+            title
+            url
+          }
         }
         ... on WpDefaultTemplate_Flexiblecontentmodules_ContentModule_LatestArticles {
           fieldGroupName
+          latestArticlesTitle
+          latestArticlesText
+          latestArticlesPostType
+          latestArticlesPostLimit
+          latestArticlesBackgroundColor
+          latestArticlesButton {
+            target
+            title
+            url
+          }
         }
         ... on WpDefaultTemplate_Flexiblecontentmodules_ContentModule_LinkBoxes {
           fieldGroupName
+          linkBoxesTitle
+          linkBoxesText
+          linkBoxesBackgroundColor
+          linkBoxesButton {
+            target
+            title
+            url
+          }
+          linkBoxes {
+            title
+            text
+            image {
+              altText
+            }
+            button {
+              target
+              title
+              url
+            }
+          }
         }
         ... on WpDefaultTemplate_Flexiblecontentmodules_ContentModule_LinkPanels {
           fieldGroupName
+          linkPanelsTitle
+          linkPanelsText
+          linkPanelsBackgroundColor
+          linkPanelsButton {
+            target
+            title
+            url
+          }
+          linkPanels {
+            title
+            text
+            image {
+              altText
+            }
+            button {
+              target
+              title
+              url
+            }
+          }
         }
         ... on WpDefaultTemplate_Flexiblecontentmodules_ContentModule_List {
           fieldGroupName
+          listTitle
+          listText
+          listBackgroundColor
+          listButton {
+            target
+            title
+            url
+          }
+          list {
+            title
+            text
+          }
         }
         ... on WpDefaultTemplate_Flexiblecontentmodules_ContentModule_Logos {
           fieldGroupName
+          logosTitle
+          logosText
+          logosBackgroundColor
+          logosButton {
+            target
+            title
+            url
+          }
+          logos {
+            altText
+          }
         }
         ... on WpDefaultTemplate_Flexiblecontentmodules_ContentModule_NavBlock {
           fieldGroupName
+          navBlockTitle
+          navBlockText
+          navBlockBackgroundColor
+          navBlockButton {
+            target
+            title
+            url
+          }
+          navBlockLinks {
+            link {
+              target
+              title
+              url
+            }
+          }
         }
         ... on WpDefaultTemplate_Flexiblecontentmodules_ContentModule_Selection {
           fieldGroupName
+          selectionTitle
+          selectionText
+          selectionBackgroundColor
+          selectionOptions {
+            optionCards {
+              optionOne {
+                title
+                text
+                link {
+                  target
+                  title
+                  url
+                }
+                image {
+                  altText
+                }
+              }
+              optionTwo {
+                title
+                text
+                link {
+                  target
+                  title
+                  url
+                }
+                image {
+                  altText
+                }
+              }
+              optionImage {
+                altText
+              }
+            }
+          }
         }
         ... on WpDefaultTemplate_Flexiblecontentmodules_ContentModule_SupportTiers {
           fieldGroupName
+          supportTiersTitle
+          supportTiersText
+          supportTiersBackgroundColor
+          supportTiersButton {
+            target
+            title
+            url
+          }
+          supportTiersCategories {
+            category
+          }
+          supportTiersFeatures {
+            featureTitle
+            featureSupport {
+              supported
+            }
+          }
         }
         ... on WpDefaultTemplate_Flexiblecontentmodules_ContentModule_Switch {
           fieldGroupName
+          switchTitle
+          switchText
+          switchBackgroundColor
+          switchOptions {
+            title
+            text
+            link {
+              target
+              title
+              url
+            }
+          }
         }
         ... on WpDefaultTemplate_Flexiblecontentmodules_ContentModule_TextArea {
           fieldGroupName
+          textAreaTitle
+          textAreaText
+          textAreaBackgroundColor
+          textAreaButton {
+            target
+            title
+            url
+          }
+          textArea {
+            title
+            text
+          }
         }
         ... on WpDefaultTemplate_Flexiblecontentmodules_ContentModule_TextBlock {
           fieldGroupName
+          textBlockTitle
+          textBlockText
+          textBlockLocation
+          textBlockBackgroundColor
+          textBlockButton {
+            target
+            title
+            url
+          }
         }
         ... on WpDefaultTemplate_Flexiblecontentmodules_ContentModule_TextImage {
           fieldGroupName
+          textImageTitle
+          textImageText
+          textImagePosition
+          textImageBackgroundColor
+          textImageButton {
+            target
+            title
+            url
+          }
+          textImage {
+            altText
+          }
         }
         ... on WpDefaultTemplate_Flexiblecontentmodules_ContentModule_ThreeColumns {
           fieldGroupName
+          threeColumnsTitle
+          threeColumnsText
+          threeColumnsBackgroundColor
+          threeColumnsButton {
+            target
+            title
+            url
+          }
+          columns {
+            title
+            text
+            link {
+              target
+              title
+              url
+            }
+            image {
+              altText
+            }
+          }
         }
         ... on WpDefaultTemplate_Flexiblecontentmodules_ContentModule_Videos {
           fieldGroupName
+          videosTitle
+          videosText
+          videosBackgroundColor
+          videosButton {
+            target
+            title
+            url
+          }
+          videos {
+            videoEmbedLink
+            text
+          }
         }
       }
     }
