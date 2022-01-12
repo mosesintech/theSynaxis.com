5154e4ab5ea31a061ff7a7512b246f51d530635b
Moses Maximino Cosme II <mosesmaxcosme@gmail.com>
2022-01-11 19:17:08 -0600

    add Post query based on post type to LatestArticle module

--- src/components/FlexibleContent/LatestArticles.tsx
+++ src/components/FlexibleContent/LatestArticles.tsx
@@ -1,6 +1,49 @@
 import React from "react"
+import { useStaticQuery, graphql } from 'gatsby'
 
-const LatestArticles = props => {
+import { FlexibleContentProps } from "../../interfaces"
+
+interface ModuleProps extends FlexibleContentProps {
+  latestArticlesTitle?: string;
+  latestArticlesText?: string;
+  latestArticlesPostType?: string;
+  latestArticlesPostLimit?: string;
+  latestArticlesBackgroundColor?: string;
+  latestArticlesButton?: {
+    target?: string;
+    title?: string;
+    url?: string;
+  }
+}
+
+const LatestArticles: React.FC<ModuleProps> = (props) => {
+  const usePostData = () => {
+    const { allWpPost } = useStaticQuery(
+      graphql`
+        query PostQuery {
+          allWpPost {
+            nodes {
+              title
+            }
+          }
+        }
+      `
+    )
+    return { allWpPost }
+  }
+
+  if(!!props.latestArticlesPostType && props.latestArticlesPostType.toLowerCase() === 'post') {
+    const allPosts = usePostData();
+
+    return (
+      <>
+        <h1>LATEST ARTICLES</h1>
+        <pre>{JSON.stringify(props, null, 2)}</pre>
+        <pre>{JSON.stringify(allPosts, null, 2)}</pre>
+      </>
+    )  
+  }
+
   return (
     <>
       <h1>LATEST ARTICLES</h1>
