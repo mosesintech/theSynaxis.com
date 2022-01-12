0c4a0735157a00cd04ed0b42db759298c60608cc
Moses Maximino Cosme II <mosesmaxcosme@gmail.com>
2022-01-11 19:00:14 -0600

    add Forms query to Form module

--- src/components/FlexibleContent/Form.tsx
+++ src/components/FlexibleContent/Form.tsx
@@ -1,10 +1,83 @@
 import React from "react"
+import { useStaticQuery, graphql } from 'gatsby'
 
-const Form = props => {
+import { FlexibleContentProps } from "../../interfaces"
+
+interface ModuleProps extends FlexibleContentProps {
+  formId?: string;
+  formTitle?: string;
+  formText?: string;
+  formBackgroundColor?: string;
+}
+
+
+const Form: React.FC<ModuleProps> = (props) => {
+  const { formId, formTitle, formText, formBackgroundColor } = props;
+
+  const useGravityData = () => {
+    const { allWpGravityFormsForm } = useStaticQuery(
+      graphql`
+        query FormQuery {
+          allWpGravityFormsForm {
+            nodes {
+              formId
+              title
+              description
+              button {
+                text
+              }
+              confirmations {
+                isDefault
+                message
+              }
+              formFields {
+                nodes {
+                id
+                type
+                formId
+                  ... on WpTextField {
+                      id
+                      label
+                      cssClass
+                      isRequired
+                      description
+                      placeholder
+                  }
+                  ... on WpEmailField {
+                      id
+                      label
+                      cssClass
+                      isRequired
+                      description
+                      placeholder
+                  }
+                  ... on WpTextAreaField {
+                      id
+                      label
+                      cssClass
+                      isRequired
+                      description
+                      placeholder
+                  }
+                }
+              }
+            }
+          }
+        }
+      `
+    )
+    return { allWpGravityFormsForm }
+  }
+
+  const { allWpGravityFormsForm } = useGravityData();
+
+  const form = allWpGravityFormsForm.nodes.find((form:any) => Number(form.formId) === Number(formId));
+
   return (
     <>
       <h1>FORM</h1>
       <pre>{JSON.stringify(props, null, 2)}</pre>
+      <pre>{JSON.stringify(form, null, 2)}</pre>
     </>
   )
 }
---
+++ src/interfaces.tsx
@@ -0,0 +1,8 @@
+import { PageProps } from 'gatsby';
+
+interface FlexibleContentProps extends PageProps {
+    title?: string
+    slug?: string
+}
+  
+export { FlexibleContentProps }
