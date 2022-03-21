4ef79da8505b84d9cf5941a293bcf7f430cc2a4d
Moses Maximino Cosme II <mosesmaxcosme@gmail.com>
2022-03-21 08:45:00 -0500

    add MobileMenu

--- src/components/Layout/menus/DesktopMenu.tsx
+++ src/components/Layout/menus/DesktopMenu.tsx
@@ -13,6 +13,8 @@ import {
 } from "@heroicons/react/outline"
 import { ChevronDownIcon } from "@heroicons/react/solid"
 
+import MobileMenu from './MobileMenu'
+
 const solutions = [
   {
     name: "Scripture Reader",
@@ -188,93 +190,7 @@ export default function DesktopMenu() {
             </div>
           </div>
 
-          <Transition
-            as={Fragment}
-            enter="duration-200 ease-out"
-            enterFrom="opacity-0 scale-95"
-            enterTo="opacity-100 scale-100"
-            leave="duration-100 ease-in"
-            leaveFrom="opacity-100 scale-100"
-            leaveTo="opacity-0 scale-95"
-          >
-            <Popover.Panel
-              focus
-              className="absolute top-0 inset-x-0 p-2 transition transform origin-top-right md:hidden"
-            >
-              <div className="rounded-lg shadow-lg ring-1 ring-black ring-opacity-5 bg-white divide-y-2 divide-gray-50">
-                <div className="pt-5 pb-6 px-5">
-                  <div className="flex items-center justify-between">
-                    <div>
-                      <img
-                        className="h-8 w-auto"
-                        src="https://tailwindui.com/img/logos/workflow-mark-indigo-600.svg"
-                        alt="Workflow"
-                      />
-                    </div>
-                    <div className="-mr-2">
-                      <Popover.Button className="bg-white rounded-md p-2 inline-flex items-center justify-center text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500">
-                        <span className="sr-only">Close menu</span>
-                        <XIcon className="h-6 w-6" aria-hidden="true" />
-                      </Popover.Button>
-                    </div>
-                  </div>
-                  <div className="mt-6">
-                    <nav className="grid gap-y-8">
-                      {solutions.map(item => (
-                        <Link
-                          key={item.name}
-                          to={item.href}
-                          className="-m-3 p-3 flex items-center rounded-md hover:bg-gray-50"
-                        >
-                          <item.icon
-                            className="flex-shrink-0 h-6 w-6 text-indigo-600"
-                            aria-hidden="true"
-                          />
-                          <span className="ml-3 text-base font-medium text-gray-900">
-                            {item.name}
-                          </span>
-                        </Link>
-                      ))}
-                    </nav>
-                  </div>
-                </div>
-                <div className="py-6 px-5 space-y-6">
-                  <div className="grid grid-cols-2 gap-y-4 gap-x-8">
-                    <Link
-                      to="#"
-                      className="text-base font-medium text-gray-900 hover:text-gray-700"
-                    >
-                      Pricing
-                    </Link>
-
-                    <Link
-                      to="#"
-                      className="text-base font-medium text-gray-900 hover:text-gray-700"
-                    >
-                      Docs
-                    </Link>
-                  </div>
-                  <div>
-                    <Link
-                      to="#"
-                      className="w-full flex items-center justify-center px-4 py-2 border border-transparent rounded-md shadow-sm text-base font-medium text-white bg-indigo-600 hover:bg-indigo-700"
-                    >
-                      Sign up
-                    </Link>
-                    <p className="mt-6 text-center text-base font-medium text-gray-500">
-                      Existing customer?{" "}
-                      <Link
-                        to="#"
-                        className="text-indigo-600 hover:text-indigo-500"
-                      >
-                        Sign in
-                      </Link>
-                    </p>
-                  </div>
-                </div>
-              </div>
-            </Popover.Panel>
-          </Transition>
+          <MobileMenu />
         </Popover>
       </Headroom>
     </>
