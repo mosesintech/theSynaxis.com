c216199ea5bb49682bbb0e81620d957eea0dfc19
Moses Maximino Cosme II <mosesmaxcosme@gmail.com>
2022-03-22 19:24:40 -0500

    update Hero component

--- src/components/FlexibleContent/Hero.tsx
+++ src/components/FlexibleContent/Hero.tsx
@@ -1,10 +1,12 @@
 import React from "react"
+import { Link } from 'gatsby'
 
 import { FlexibleContentProps } from "../../interfaces"
 import Edges from "../Layout/Edges"
 
 export interface HeroProps extends FlexibleContentProps {
   heroTitle?: string
+  heroSubtitle?: string
   heroText?: string
   heroImage?: {} // Add to WordPress
   heroBackgroundColor?: string
@@ -16,14 +18,62 @@ export interface HeroProps extends FlexibleContentProps {
     title?: string
     url?: string
   }
+  heroSecondaryButton?: {
+    target?: string
+    title?: string
+    url?: string
+  }
 }
 
 const Hero: React.FC<HeroProps> = props => {
+  const { heroTitle, heroSubtitle, heroText, heroButton, heroSecondaryButton } = props;
   return (
     <>
       <Edges size="lg">
-        <h1>HERO</h1>
-        <pre>{JSON.stringify(props, null, 2)}</pre>
+        <div className="relative bg-white-50 overflow-hidden">
+          <div className="relative pt-6 pb-16 sm:pb-24">
+            <main className="mt-16 mx-auto max-w-7xl px-4 sm:mt-24">
+              <div className="text-center">
+                  <h1 className="text-4xl tracking-tight font-extrabold font-headingStyled text-gray-900 sm:text-5xl md:text-6xl">
+                    {heroTitle && (
+                      <span className="block xl:inline">{heroTitle}</span>
+                    )}
+                    {heroSubtitle && (
+                      <span className="block text-indigo-600 xl:inline">{heroSubtitle}</span>
+                    )}
+                  </h1>
+                {heroText && (
+                  <p 
+                    className="mt-3 max-w-md mx-auto text-base text-gray-500 sm:text-lg md:mt-5 md:text-xl md:max-w-3xl"
+                    children={heroText}
+                  />
+                )}
+                <div className="mt-5 max-w-md mx-auto sm:flex sm:justify-center md:mt-8">
+                  {heroButton && (
+                    <div className="rounded-md shadow">
+                      <Link
+                        to={`${heroButton.url}`}
+                        className="w-full flex items-center justify-center px-8 py-3 border border-transparent text-base font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 md:py-4 md:text-lg md:px-10"
+                      >
+                        {heroButton.title}
+                      </Link>
+                    </div>
+                  )}
+                  {heroSecondaryButton && (
+                    <div className="mt-3 rounded-md shadow sm:mt-0 sm:ml-3">
+                      <Link
+                        to={`${heroSecondaryButton.url}`}
+                        className="w-full flex items-center justify-center px-8 py-3 border border-transparent text-base font-medium rounded-md text-indigo-600 bg-white hover:bg-gray-50 md:py-4 md:text-lg md:px-10"
+                      >
+                        {heroSecondaryButton.title}
+                      </Link>
+                    </div>
+                  )}
+                </div>
+              </div>
+            </main>
+          </div>
+        </div>
       </Edges>
     </>
   )
