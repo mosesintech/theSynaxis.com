10efcfa855451eef92c88e5312d47b38f9143059
Moses Maximino Cosme II <mosesmaxcosme@gmail.com>
2022-04-13 10:14:11 -0500

    add dotenv

--- src/apollo/client.js
+++ src/apollo/client.js
@@ -1,6 +1,10 @@
 import fetch from "cross-fetch"
 import { ApolloClient, InMemoryCache, HttpLink } from "@apollo/client"
 
+require(`dotenv`).config({
+  path: `.env.${process.env.NODE_ENV}`,
+})
+
 export const client = new ApolloClient({
   link: new HttpLink({
     uri: process.env.WPGRAPHQL_URL,
