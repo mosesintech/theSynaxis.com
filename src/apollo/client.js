3ad19de5928c63d23ce282174bf20aa3c3ea2c5b
Moses Maximino Cosme II <mosesmaxcosme@gmail.com>
2022-04-13 10:44:56 -0500

    format

--- src/apollo/client.js
+++ src/apollo/client.js
@@ -1,7 +1,7 @@
 import fetch from "cross-fetch"
 import { ApolloClient, InMemoryCache, HttpLink } from "@apollo/client"
 
-const graphqlHost = process.env.REACT_APP_WPGRAPHQL_URL;
+const graphqlHost = process.env.REACT_APP_WPGRAPHQL_URL
 
 export const client = new ApolloClient({
   link: new HttpLink({
