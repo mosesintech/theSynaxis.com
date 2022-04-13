f35eef4b7f866b1a348ecf6e0cd3c1e57132d86f
Moses Maximino Cosme II <mosesmaxcosme@gmail.com>
2022-04-13 11:34:59 -0500

    add production check to apollo link env variable

--- src/apollo/client.js
+++ src/apollo/client.js
@@ -1,7 +1,10 @@
 import fetch from "cross-fetch"
 import { ApolloClient, InMemoryCache, createHttpLink } from "@apollo/client"
 
-const graphqlHost = `${process.env.REACT_APP_WPGRAPHQL_URL}`
+const graphqlHost =
+  process.env.NODE_ENV === "production"
+    ? `${process.env.GATSBY_WPGRAPHQL_URL}`
+    : `${process.env.WPGRAPHQL_URL}`
 
 export const link = createHttpLink({
   uri: graphqlHost,
